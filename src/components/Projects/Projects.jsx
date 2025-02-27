144b884bb90cae1148d365510ae0a2cf3c49d0a3
Nikitha-gr <nikitha14.gr@gmail.com>
2025-02-27 20:59:24 +0530

    Updated Project Home Page

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import styles from "./Projects.module.css";
-import Seara_Project from "../../assets/Seara_Project.png";
+import ShopSphere_Project from "../../assets/ShopSphere_Project.png";
 
 export const Projects = () => {
     return (
         <section className={styles.container}>
             <h1 className={styles.title}>Projects</h1>
             <div className={styles.projectCard}>
-                <h2 className={styles.projectTitle}>Seara</h2>
+                <h2 className={styles.projectTitle}>ShopSphere</h2>
                 <p className={styles.description}>
-                    Seara is a responsive web application designed for seamless navigation across all devices,
+                    ShopSphere is a responsive web application designed for seamless navigation across all devices,
                     including desktop 🖥️, tablet 📱, and mobile 📲. The project includes a product showcase 🛍️,
                     a newsletter subscription form 📬, a cart feature 🛒, and categorized sections for products under $50 💵,
                     under $100 💰, and for kids 🧸.
                 </p>
 
                 <div className={styles.content}>
-                    <img src={Seara_Project} alt="Seara Project Homepage" className={styles.projectImg} />
+                    <img src={ShopSphere_Project} alt="ShopSphere Project Homepage" className={styles.projectImg} />
                 </div>
 
                 {/* Skills Section */}
@@ -27,10 +27,10 @@ export const Projects = () => {
                 </ul>
 
                 <div className={styles.links}>
-                    <a href="https://nikitha-gr.github.io/Seara/" className={styles.link} target="_blank" rel="noopener noreferrer">
+                    <a href="https://nikitha-gr.github.io/ShopSphere/" className={styles.link} target="_blank" rel="noopener noreferrer">
                         Demo
                     </a>
-                    <a href="https://github.com/Nikitha-gr/Seara.git" className={styles.link} target="_blank" rel="noopener noreferrer">
+                    <a href="https://github.com/Nikitha-gr/ShopSphere.git" className={styles.link} target="_blank" rel="noopener noreferrer">
                         Source
                     </a>
                 </div>
